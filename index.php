441d90e10c7f24914632b2668bc0c886105c4f1c
wenyiwen <645228905@qq.com>
2017-11-28 14:22:58 +0800

    添加功能模块

--- index.php
+++ index.php
@@ -7,10 +7,23 @@
 </head>
 <body>
 <div id="main">
-    <div id="header"></div>
-    <div id="message"></div>
+    <div id="header">
+        <h1>Text</h1>
+    </div>
+    <div id="message">
+        <form id="form" method="post" action="insert.php">
+            <label for="name">Your name:</label> <br>
+            <input id="name" name="user" type="text" ><br>
+            <label for="mess">You must want to say something,Please input your message,thanks!!! </label><br>
+            <textarea id="mess" name="message" ></textarea><br>
+            <input id="send" type="submit" value="sand" >
+        </form>
+    </div>
     <div id="content"></div>
-    <div id="foot"></div>
+    <div id="foot">
+        <p>welcome to my books,thanks!</p>
+        <p>by @wenyiwen</p>
+    </div>
 </div>
 </body>
 </html>
---
+++ insert.php
@@ -0,0 +1,9 @@
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wen
+ * Date: 2017/11/28
+ * Time: 11:36
+ */
+echo "succse!!!"."</br><a href=\"index.php\">back my home</a>";
