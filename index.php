43ec6199c3462bae775fa7908c06488f2e0d53ad
wenyiwen <645228905@qq.com>
2017-11-28 11:22:45 +0800

    前端布局

--- index.php
+++ index.php
@@ -2,14 +2,15 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Title</title>
+    <title> welcome my Book</title>
     <link rel="stylesheet" href="mystyle.css" type="text/css" >
 </head>
 <body>
 <div id="main">
-    <div id="header">
-        
-    </div>
+    <div id="header"></div>
+    <div id="message"></div>
+    <div id="content"></div>
+    <div id="foot"></div>
 </div>
 </body>
 </html>
